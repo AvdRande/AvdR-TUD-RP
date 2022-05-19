b7a00b9b4d6b71163013ac30c7aa79d8f2fb7574
Arend van der Rande <a.c.vanderrande@student.tudelft.nl>
2022-05-19 18:35:32 +0200

    Poeheee wat een code wat een dag So I updated the model, a lot, HMC-LMLP still doesn't work as desired

--- recommender/Notes on the recommenders.md
+++ recommender/Notes on the recommenders.md
@@ -1,4 +1,7 @@
 Notes about current version of HMC-LMLP:
+If I read the paper correctly, especially section 3.2, the predictions are not rounded
+for training, but they are for the final predictions. Does this make sense?
+
 When using 100 samples, unlimited features, max_iter of 1000, there are many 
 predictions which are good ish. Certainly at least 30% where some are entirely correct
 
@@ -6,3 +9,6 @@ When using 10000 samples, 1500 max features, max iter of 100, it often doesn't
 predict anything. When it does, it also isn't always correct. Also, the longest 
 training time is for the first layer, as there are many features. I believe
 the max amount of features is around 15000.
+
+Hypothesis: maybe the predictions are "bad" because we go from 2500 features to 7 
+and then back up to 220. The information just cannot carry well through so little.
--- recommender/hmc-lmlp-classifier.py
+++ recommender/hmc-lmlp-classifier.py
@@ -2,13 +2,14 @@ from functools import partial
 from pprint import pprint
 
 import click
-import fasttext
 import numpy as np
 import pandas as pd
+from regex import E
 import sklearn
 from scipy import stats
 import json
 import random
+import pickle
 
 from sklearn.datasets import make_multilabel_classification
 from sklearn.multioutput import MultiOutputClassifier
@@ -30,58 +31,136 @@ def ft(trainf, testf, hierarchyf, labels_column, readme_column, model_output, le
     train = pd.read_csv(trainf)
     test = pd.read_csv(testf)
 
-    limiter = 10000
+    train_limiter = 4000
+    test_limiter = 1000
 
-    train_features, train_labels = df2feature_class(train, limiter, readme_column, labels_column)
+    print("Now converting training csv to features and labels")
+    train_features, train_labels = df2feature_class(train, train_limiter, readme_column, labels_column)
+    
+    print("Now converting testing csv to features and labels")
+    test_features, test_labels = df2feature_class(test, test_limiter, readme_column, labels_column)
 
-    # convert features to vectors using tfidf
-    vectorizer = TfidfVectorizer(min_df=5, max_df=0.7, max_features=2500)
-    X = vectorizer.fit_transform([" ".join(train_feature) for train_feature in train_features]).toarray()
+    def features_to_vectors(features_list):
+        vectorizer = TfidfVectorizer(min_df=5, max_features=2500)
+        all_features = [" ".join(f) for f in (features_list[0] + features_list[1])]
+        vectors = vectorizer.fit_transform(all_features).toarray()
+        return vectors[:len(features_list[0])], vectors[len(features_list[0]):]
+
+    train_feature_vector, test_feature_vector = features_to_vectors([train_features, test_features])
 
     label_names = np.array(train.columns[:-2])
 
     if model_output == "LR":
         # classify using logistic regression
-        clf = MultiOutputClassifier(LogisticRegression()).fit(X, train_labels)
-        result = clf.predict(X[:2])
+        clf = MultiOutputClassifier(LogisticRegression()).fit(train_feature_vector, train_labels)
+        # result = clf.predict(train_feature_vector[:2])
+        test_predictions = clf.predict(test_feature_vector)
+
+        show_prec_rec_atn(test_predictions, test_labels)
+        
     elif model_output == "HMC-LMLP":
         with open(hierarchyf) as f:
+
+
+            print("Start training HMC-LMLP")
             hierarchy = json.load(f)
 
-            i_hier = build_idx_hierarchy(hierarchy, label_names)
+            # i_hier = build_idx_hierarchy(hierarchy, label_names)
 
-            y = [labels_to_subtree(i_hier, tl) for tl in train_labels]
+            # print("Building subtrees for all of the labels")
+            # label_subtrees = [labels_to_subtree(i_hier, tl) for tl in train_labels]
 
             depth = tree_depth(hierarchy)
             nn_layers = []
             predictions = []
-            for level in range(depth):
-                print("Training HMC-LMLP layer ", level)
-                # the input matrix for this neural network
-                feature_matrix = X if level == 0 else predictions[level - 1]
-                # the expected outcome for this neural network
-                target_labels = [labels_at_level(yiter, level) for yiter in y]
-                # how many hidden layers?
-                n_hidden_layers = max(min(100,len(feature_matrix[0])), len(target_labels[0]))
-                # make a new classifier for this level of the hierarchy
-                nn_layers.append(MLPClassifier(solver='lbfgs', hidden_layer_sizes=n_hidden_layers, random_state=1, max_iter=100))
-                # train the neural network
-                print("Neural network consists of three layers: ", len(feature_matrix[0]), " to ", n_hidden_layers, " to ", len(target_labels[0]))
-                nn_layers[level].fit(feature_matrix, target_labels)
-                # add results to predictions
-                print("Fitting done, calculating predictions for next layer")
-                if level == depth - 1:
-                    predictions.append(nn_layers[level].predict(feature_matrix))
-                else:
-                    predictions.append(nn_layers[level].predict_proba(feature_matrix))
 
-            print("Training done, now showing result:")
-            actual = [np.array(labels_at_level(labels_to_subtree(i_hier, train_label), depth - 1)) for train_label in train_labels[:100]]
-            for i in range(100):
-                print("Prediction: ", binlabels_to_text(predictions[depth - 1][i], label_names))
-                print("Actual: " , binlabels_to_text(actual[i], label_names))
-                print("Difference: ", sum(np.abs(predictions[depth - 1][i] - np.array(actual[i]))))
-                input()
+            do_train = False
+            filename = 'hmc-lmlp-model.sav'
+            try:
+                with open(filename, 'rb') as model_dump:
+                    if input("Do you want to load the previous model?(y)") == "y":
+                        nn_layers = pickle.load(model_dump)
+                    else:
+                        do_train = True
+            except EnvironmentError:
+                do_train = True
+
+            if do_train:
+                for level in range(depth):
+                    print("Training HMC-LMLP layer ", level)
+                    # the input matrix for this neural network
+                    feature_matrix = train_feature_vector if level == 0 else predictions[level - 1]
+                    # the expected outcome for this neural network
+                    # target_labels = [labels_at_level(subtree, level) for subtree in label_subtrees] <- old method
+                    target_labels = [target_labels_at_level(binlabels_to_text(label, label_names), hierarchy, level) for label in train_labels]
+                    # how many hidden layers?
+                    n_hidden_layers = max(min(100, len(feature_matrix[0])), len(target_labels[0]))
+                    # make a new classifier for this level of the hierarchy
+                    nn_layers.append(MLPClassifier(solver='lbfgs', hidden_layer_sizes=n_hidden_layers, random_state=1, max_iter=1000))
+                    # train the neural network
+                    print("Neural network consists of three layers: ", len(feature_matrix[0]), " to ", n_hidden_layers, " to ", len(target_labels[0]))
+                    nn_layers[level].fit(feature_matrix, target_labels)
+                    # add results to predictions
+                    print("Fitting done, calculating predictions for next layer")
+                    if level < depth - 1:
+                        predictions.append(nn_layers[level].predict_proba(feature_matrix))
+
+            if(input("Do you want to save the model?(y)") == "y"): 
+                pickle.dump(nn_layers, open(filename, 'wb'))
+
+            print("Training done, now showing test results: ")
+            # test_predictions = nn_layers[3].predict(nn_layers[2].predict(nn_layers[1].predict(nn_layers[0].predict(test_feature_vector))))
+            intermediate_predictions = []
+            intermediate_predictions.append(nn_layers[0].predict_proba(test_feature_vector))
+            for i in range(1, depth):
+                intermediate_predictions.append(nn_layers[i].predict_proba(intermediate_predictions[-1]))
+            test_predictions = intermediate_predictions[-1]
+
+            # something to check, or postprocess: ensure that the hierarchical constrains are in fact obeyed
+
+            show_prec_rec_atn(test_predictions, [target_labels_at_level(binlabels_to_text(test_label, label_names), hierarchy, depth-1) for test_label in test_labels], 1)
+
+            show_prec_rec_atn(test_predictions, [target_labels_at_level(binlabels_to_text(test_label, label_names), hierarchy, depth-1) for test_label in test_labels], 3)
+
+            show_prec_rec_atn(test_predictions, [target_labels_at_level(binlabels_to_text(test_label, label_names), hierarchy, depth-1) for test_label in test_labels], 5)
+
+
+def show_prec_rec_atn(predictions, true_values, n):
+    true_positives = 0
+    false_positives = 0
+    false_negatives = 0
+    for i in range(len(predictions)):
+        top_n_label_idxs = np.argpartition(predictions[i], -n)[-n:]
+        for idx in top_n_label_idxs:
+            if true_values[idx] == 1:
+                true_positives += 1
+            else:
+                false_positives += 1
+    print("Precision@", n, ": ", true_positives / (true_positives + false_positives))
+
+def target_labels_at_level(str_labels, hierarchy, level):
+    if level == 0:
+        # go into children and find in which children the labels are
+        ret = []
+        if len(hierarchy["children"]) == 0:
+            for tag in hierarchy["content"]:
+                ret.append(1 if tag in str_labels else 0)
+        else:
+            for child in hierarchy["children"]:
+                any_label_found = False
+                for label in str_labels:
+                    if is_label_in_tree(label, child):
+                        any_label_found = True
+                ret.append(1 if any_label_found else 0)
+        return ret
+    else:
+        ret = []
+        for child in hierarchy["children"]:
+            ret += target_labels_at_level(str_labels, child, level - 1)
+        return ret
+
+def is_label_in_tree(label, hierarchy):
+    return label in hierarchy["content"] or any([is_label_in_tree(label, child) for child in hierarchy["children"]])
 
 # convert hierarchy to hierarchy of indexes of the labels
 def build_idx_hierarchy(hierarchy, label_names):
@@ -132,7 +211,7 @@ def tree_depth(tree):
 def labels_at_level(idx_hierarchy, level):
     if level == 0:
         if len(idx_hierarchy["children"]) == 0:
-            return idx_hierarchy["content"] # CONTINUE HERE STUPID ME!
+            return idx_hierarchy["content"]
         else:
             return np.array([1 if 1 in sub["content"] else 0 for sub in idx_hierarchy["children"]])
     if level > 0:
@@ -142,6 +221,15 @@ def labels_at_level(idx_hierarchy, level):
             for t in temp:
                 ret.append(t)
         return ret
+
+def get_leaves(hierarchy):
+    ret = []
+    if len(hierarchy["children"]) == 0:
+        return hierarchy["content"]
+    else:
+        for child in hierarchy["children"]:
+            ret += get_leaves(child)
+    return ret
 
 def binlabels_to_text(binlabels, label_names):
     ret = []
@@ -167,9 +255,6 @@ def df2feature_class(dataframe, n, feature_column, label_column):
                 if x[0].isnumeric():
                     dupe.append(int(x[0]))
         df_labels.append(np.array(dupe))
-
-        if random.uniform(0, 1) < 100/total:
-            print(round(running / total,2))
         running += 1
         if running > total - 1:
             break
